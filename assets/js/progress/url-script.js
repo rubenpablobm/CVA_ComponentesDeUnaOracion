b9776857d3d68481cfcf9111868f58ebec30e4f9
rubenpablobm <rubenp.barrazam@gmail.com>
2022-11-15 08:02:52 -0600

    Agrega página mod4-12 con su contenido

--- assets/js/progress/url-script.js
+++ assets/js/progress/url-script.js
@@ -249,6 +249,7 @@ function getFiles() {
             9: "modulo4-9.html",
             10: "modulo4-10.html",
             11: "modulo4-11.html",
+            12: "modulo4-12.html",
         },
         modulo5: {
             1: "modulo5-1.html",
